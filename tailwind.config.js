d9d83e68a98fa88ed02adfa86566850fe5fb5d05
MAHMUD SANJID <sanjidk24@gmail.com>
2022-07-23 02:22:52 -0700

    create react project and install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary:"#570DF8",
+          secondary:"#F000B8",
+          accent:"#37CDBE",
+          neutral:"#3D4451",
+          success:"#36D399",
+          warning:"#FBBD23",
+          error:"#F87272",
+          "base-100":"#FFFFFF",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
